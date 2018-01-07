5687954a22ab47b79aa8497a20125100e7c48356
TomIrimescu <tom@irimescu.com>
2018-01-07 16:33:41 -0500

    prettified .js files

--- src/helper/index.test.js
+++ src/helper/index.test.js
@@ -1,14 +1,14 @@
-import { max_number } from './index';
+import { max_number } from "./index";
 
-describe('max_number', () => {
-  describe('given an empty array', () => {
-    it('returns 0', () => {
+describe("max_number", () => {
+  describe("given an empty array", () => {
+    it("returns 0", () => {
       expect(max_number([])).toEqual(0);
     });
   });
-  
-  describe('given an array of numbers', () => {
-    it('returns the max number', () => {
+
+  describe("given an array of numbers", () => {
+    it("returns the max number", () => {
       expect(max_number([1, 2, 3])).toEqual(3);
     });
   });
